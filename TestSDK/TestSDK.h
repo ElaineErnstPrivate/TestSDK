1baa171b215905b487bf3161a6ace8bcb3e728f7
Elaine Ernst <elainee@globalkinetic.com>
2019-12-11 16:57:55 +0200

    Initial Commit

---
+++ TestSDK/TestSDK.h
@@ -0,0 +1,19 @@
+//
+//  TestSDK.h
+//  TestSDK
+//
+//  Created by Elaine Ernst on 2019/12/11.
+//  Copyright © 2019 Global Kinetic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSDK.
+FOUNDATION_EXPORT double TestSDKVersionNumber;
+
+//! Project version string for TestSDK.
+FOUNDATION_EXPORT const unsigned char TestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSDK/PublicHeader.h>
+
+
