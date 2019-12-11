0f9bdbcb61611c0bdd38b8886f484b37bcaa5288
Elaine Ernst <elainee@globalkinetic.com>
2019-12-11 17:18:15 +0200

    First remote

---
+++ TestSDK/SomeRandomShit.swift
@@ -0,0 +1,18 @@
+//
+//  SomeRandomShit.swift
+//  TestSDK
+//
+//  Created by Elaine Ernst on 2019/12/11.
+//  Copyright © 2019 Global Kinetic. All rights reserved.
+//
+
+import Foundation
+
+open class SomeRandomShit{
+    
+    public func printShit(string: String){
+
+        print(string)
+    }
+    
+}
